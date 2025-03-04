4c048b5b1d5e4a5bcd11ef35c71783f09810e147
Robb Strick <stricklandmba2011@gmail.com>
2025-03-04 16:53:44 -0500

    first upload

---
+++ src/connection.ts
@@ -0,0 +1,30 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+import pg from 'pg';
+const { Client } = pg;
+
+
+const client = new Client({
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined
+
+});
+
+const connectToDb = async () => {
+    try {
+        await client.connect();
+        console.log('Connected to the database');
+    } catch (error) {
+      console.error('Error connecting to the database', error);
+      process.exit(1);
+    }
+}
+
+client.connect();
+
+export { client, connectToDb };
