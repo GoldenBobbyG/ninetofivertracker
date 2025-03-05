e9e03f36b9f8f443840d19a474cb28a1a12ed948
Robb Strick <stricklandmba2011@gmail.com>
2025-03-05 13:14:37 -0500

    clean up repo

--- dist/server.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { connectToDb } from './connection.js';
-await connectToDb();
-const PORT = process.env.PORT || 3000;
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
