5e7cc13b8bf4a3b4850fba9c47c2e2e38b107d1c
Robb Strick <stricklandmba2011@gmail.com>
2025-03-05 08:25:51 -0500

    typescript complete

--- dist/connection.js
+++ dist/connection.js
@@ -1,30 +1,24 @@
 import dotenv from 'dotenv';
 dotenv.config();
-
-
 import pg from 'pg';
 const { Client } = pg;
 console.log(process.env.DB_PASSWORD);
-
 const client = new Client({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     host: process.env.DB_HOST,
     database: process.env.DB_DATABASE,
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined
-
 });
-
 const connectToDb = async () => {
     try {
         await client.connect();
         console.log('Connected to the database');
-    } catch (error) {
-      console.error('Error connecting to the database', error);
-      process.exit(1);
     }
-}
-
+    catch (error) {
+        console.error('Error connecting to the database', error);
+        process.exit(1);
+    }
+};
 client.connect();
-
 export { client, connectToDb };
